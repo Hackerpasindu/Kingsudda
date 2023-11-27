807e34ce5dee6861fcba3d8b3fc0079585be31e1
Pasidu <152088727+Hackerpasindu@users.noreply.github.com>
2023-11-27 20:05:29 +0530

    Add files via upload

---
+++ king.py
@@ -0,0 +1,16 @@
+import os
+import time
+import sys
+# Set color
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.003)
+delay_print
+delay_print (""+G+"╔════════════════════════════════════════╗  ""\n")
+delay_print (""+G+"║ Instagram: @ 0king.sudds0                ║  ""\n")
+delay_print (""+G+"║ Github: https://github.com/pasiya-X/ ║  ""\n")
+delay_print (""+G+"╚════════════════════════════════════════╝  ""\n")
